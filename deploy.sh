68424bb50b84f66dadb745f9f27b8eb73634d17a
Nicolas Barray <nicolas.barray@infinit.io>
2015-11-21 19:45:59 +0100

    vim: add color scheme

--- deploy.sh
+++ deploy.sh
@@ -7,8 +7,13 @@ git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 
 echo 'vim: copy vimrc'
 cp templates/vimrc ~/.vimrc
-echo 'vim: plugin install'
-vim +PluginInstallq
+
+# FIXME: How to install vundle plugin without having to start vim ?
+# echo 'vim: plugin install'
+# vim +PluginInstallq
+
+echo 'vim: add color scheme file'
+cp templates/jellybeans.vim ~/.vim/colors/
 
 echo 'copy gitconfig'
 cp templates/gitconfig ~/.vimrc
